fffba79a54ea1e17a3589aee42558bbf54732327
JUNGI JEONG <junejjk@gmail.com>
2023-08-21 15:03:38 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
