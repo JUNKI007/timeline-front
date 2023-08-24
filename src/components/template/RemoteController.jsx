b262741499ce9d641ae7916ea51e75e2b16ea585
khdaragon1102 <khdragon1102@gmail.com>
2023-08-24 13:42:05 +0900

    paosting working

--- src/components/template/RemoteController.jsx
+++ src/components/template/RemoteController.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './RemoteController.scss';
 import Post from '../post/Post';
 
+
 const RemoteController = ({ setPostModalOpen }) => {
   const handleScrollToTop = () => {
     window.scrollTo(0, 0);
@@ -15,10 +16,10 @@ const RemoteController = ({ setPostModalOpen }) => {
   return (
     <div className="remote-controller">
       <div className="remote-controller-inner">
-        <button onClick={handleScrollToTop}>Top</button>
+        <button onClick={() => handleScrollToTop()}>Top</button>
         <Link to="/user">My Post</Link>
         <Link to="/timeline">Timeline</Link>
-        <button onClick={RemoteControllerWritePost}>Write Post</button>
+        <button onClick={() => RemoteControllerWritePost()}>Write Post</button>
       </div>
     </div>
   );
